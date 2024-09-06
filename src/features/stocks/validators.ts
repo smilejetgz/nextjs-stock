5d9b166fa4c0df9568546e052237793d964ae06f
Tirajet Chukleang <smilejettt@hotmail.com>
2024-09-06 22:05:27 +0700

    Update validators.ts

--- src/features/stocks/validators.ts
+++ src/features/stocks/validators.ts
@@ -5,14 +5,14 @@ export const add = z.object({
   name: z
     .string({ required_error: 'Name is required' })
     .min(1, 'Name cannot be empty'),
-  amount: z.number().min(1),
+  amount: z.coerce.number().min(1),
   detail: z
     .string({ required_error: 'Detail is required' })
     .min(1, 'Detail cannot be empty'),
   status: z.enum(['APPROVED', 'REJECTED'], {
     required_error: 'Status is required',
   }),
-  CategoryId: z.number({ required_error: 'Category ID is required' }),
+  CategoryId: z.coerce.number(),
   image,
 });
 
