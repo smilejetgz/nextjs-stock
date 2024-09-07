ceee65909d48385c4808b20c44c49daf48d32d12
Tirajet Chukleang <smilejettt@hotmail.com>
2024-09-07 21:28:07 +0700

    Update dashboard/type.ts

--- src/features/dashboard/type.ts
+++ src/features/dashboard/type.ts
@@ -1,6 +1,9 @@
 import {
   type StockCountStatus,
   type StockCountByCategory,
+  type UserCount,
+  type CategoryCount,
+  type StockCount,
 } from '@/features/dashboard/api';
 
 export type StockCountByCategory = Awaited<
@@ -10,3 +13,9 @@ export type StockCountByCategory = Awaited<
 export type StockCountStatus = Awaited<
   ReturnType<typeof StockCountStatus>
 >[number];
+
+export type UserCount = Awaited<ReturnType<typeof UserCount>>;
+
+export type CategoryCount = Awaited<ReturnType<typeof CategoryCount>>;
+
+export type StockCount = Awaited<ReturnType<typeof StockCount>>;
